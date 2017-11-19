2569df7d0342e584d5ca090408fe1cdae90008d4
Stefan Schallenberg <nafets227@users.noreply.github.com>
2017-11-19 20:14:11 +0000

    Improved Script for local build

--- make-local.sh
+++ make-local.sh
@@ -5,10 +5,29 @@
 # (C) 2017 Stefan Schalelnberg
 #
 
-for f in $(ls); do
-	test -d $f && 
+function build_local () {
+	printf "Building %s ... \n" "$f"
 	docker build -t nafets227/$f $f
-	if [ $? -ne 0 ]; then 
-		exit $?
-	fi
-done
+	rc=$?; if [ $rc -ne 0 ]; then return $rc ; fi
+}
+
+if [ $# -eq 0 ] ; then
+	for f in $(ls); do
+		if [ -d $f ] ; then
+			build_local $f
+			rc=$?
+			if [ $rc -ne 0 ]; then 
+				exit $rc
+			fi
+		fi
+	done
+else
+	for f in "$@" ; do
+		build_local $f
+		rc=$?
+		if [ $rc -ne 0 ]; then 
+			exit $r
+		fi
+	done
+fi
+
